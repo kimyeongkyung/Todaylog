c47b26bebcb418e0f8c909c2fae681461ac6e38b
kimyeongkyung <klk17625@naver.com>
2023-10-25 00:10:28 +0900

    아이콘 추가

---
+++ src/components/icons/Search/index.tsx
@@ -0,0 +1,25 @@
+const Search = () => {
+  return (
+    <svg
+      width="19"
+      height="19"
+      viewBox="0 0 19 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="8.5"
+        cy="8.5"
+        r="7.5"
+        fill="white"
+        stroke="#FFC700"
+        stroke-width="2"
+      />
+      <path
+        d="M17.1718 18.172C17.5623 18.5625 18.1955 18.5625 18.586 18.172C18.9765 17.7814 18.9765 17.1483 18.586 16.7577L17.1718 18.172ZM13.1718 14.172L17.1718 18.172L18.586 16.7577L14.586 12.7577L13.1718 14.172Z"
+        fill="#FFC700"
+      />
+    </svg>
+  );
+};
+export default Search;
---
+++ src/components/icons/index.ts
@@ -0,0 +1,23 @@
+import Duck from "./Duck";
+import ShowEyes from "./ShowEyes";
+import DarkWhere from "./DarkWhere";
+import Where from "./Where";
+import DarkMypage from "./DarkMypage";
+import Mypage from "./Mypage";
+import DarkWhat from "./DarkWhat";
+import What from "./What";
+import Search from "./Search";
+import SmallHeart from "./SmallHeart";
+
+export {
+  Duck,
+  ShowEyes,
+  DarkWhere,
+  Where,
+  DarkMypage,
+  Mypage,
+  DarkWhat,
+  What,
+  Search,
+  SmallHeart,
+};
