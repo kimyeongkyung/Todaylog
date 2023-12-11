f2de3168de518efb47739a28ccb48749718f85ae
kimyeongkyung <klk17625@naver.com>
2023-12-11 16:48:03 +0900

    SquareButton 컴포넌트 추가

---
+++ src/components/Button/SquareButton/index.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+  color?: string;
+  bgColor?: string;
+  borderColor?: string;
+  margin?: string;
+}
+
+const SquareButton = ({
+  text,
+  color,
+  bgColor,
+  borderColor,
+  margin,
+}: {
+  text: string;
+  color?: string;
+  bgColor?: string;
+  borderColor?: string;
+  margin?: string;
+}) => {
+  return (
+    <Button
+      margin={margin}
+      color={color}
+      bgColor={bgColor}
+      borderColor={borderColor}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default SquareButton;
+
+const Button = styled.div<ButtonProps>`
+  width: 79px;
+  padding: 8px 0;
+  color: ${(props) => props.color || "#fff"}; /* 기본값은 흰색(#fff)으로 설정 */
+  background-color: ${(props) => props.bgColor || "#000"};
+  border: 1px solid ${(props) => props.borderColor || "transparent"};
+  margin: ${(props) => props.margin || "0 0 0 0"};
+  border-radius: 5px;
+  text-align: center;
+  font-size: 12px;
+  box-sizing: border-box;
+`;
