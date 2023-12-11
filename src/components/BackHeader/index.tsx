eb56d732dfac83edecb128f107ef875f08c466a9
kimyeongkyung <klk17625@naver.com>
2023-12-11 16:43:24 +0900

    BackHeader 컴포넌트 추가

---
+++ src/components/BackHeader/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { ArrowLeft } from "../icons";
+import { useRouter } from "next/router";
+
+const BackHeader = ({ title }: { title: string }) => {
+  const { push } = useRouter();
+
+  return (
+    <>
+      <Back
+        onClick={() => {
+          push("/");
+        }}
+      >
+        <ArrowLeft />
+      </Back>
+      <Title>{title}</Title>
+    </>
+  );
+};
+
+export default BackHeader;
+
+const Back = styled.div`
+  position: absolute;
+  top: 16px;
+  left: 16px;
+  cursor: pointer;
+`;
+const Title = styled.div`
+  width: 100%;
+  font-size: 16px;
+  padding: 0 0 16px 0;
+  font-weight: bold;
+  text-align: center;
+`;
