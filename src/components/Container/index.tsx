ffe17a4a34d09b3f9c0974ba17edbfa40d18ced7
kimyeongkyung <klk17625@naver.com>
2023-12-11 16:45:17 +0900

    Container 컴포넌트 추가

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+  justify-content: space-between;
+  padding: 16px 16px 70px 16px;
+  .title {
+    padding: 30px 0 8px 0;
+    font-weight: bold;
+  }
+  min-height: 100vh;
+  overflow: hidden;
+  margin: 52px 0 0 0;
+`;
+
+export default Container;
