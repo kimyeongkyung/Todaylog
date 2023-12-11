7ebfb47258f83bae48362df146aed6646bdb4048
kimyeongkyung <klk17625@naver.com>
2023-12-11 16:42:45 +0900

    icons 추가

--- src/components/icons/ShowEyes/index.tsx
+++ src/components/icons/ShowEyes/index.tsx
@@ -10,7 +10,7 @@ const ShowEyes = () => {
       <path
         d="M13.7124 12.8192C9.85124 13.4012 5.25845 12.453 0.685215 8.18401C2.43951 6.17468 5.55164 3.87617 9.30388 3.18316C13.1516 2.47251 17.743 3.43593 22.3285 8.30045C20.5887 10.2199 17.4805 12.2512 13.7124 12.8192Z"
         stroke="#FFC700"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <path
         d="M15.5 8C15.5 10.5361 13.6609 12.5 11.5 12.5C9.33911 12.5 7.5 10.5361 7.5 8C7.5 5.46386 9.33911 3.5 11.5 3.5C13.6609 3.5 15.5 5.46386 15.5 8Z"
@@ -22,17 +22,17 @@ const ShowEyes = () => {
         x2="6.58536"
         y2="1.61272"
         stroke="#FFC700"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <path
         d="M3.86232 5.08122L2.5 3.5"
         stroke="#FFC700"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <path
         d="M18.4059 5.04806L20 3.5"
         stroke="#FFC700"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <line
         x1="0.5"
@@ -41,9 +41,9 @@ const ShowEyes = () => {
         y2="-0.5"
         transform="matrix(0.258819 -0.965926 -0.965926 -0.258819 15 3.93262)"
         stroke="#FFC700"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
-      <path d="M11.5 2.5L11.5 1" stroke="#FFC700" stroke-linecap="round" />
+      <path d="M11.5 2.5L11.5 1" stroke="#FFC700" strokeLinecap="round" />
     </svg>
   );
 };
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -8,9 +8,25 @@ import DarkWhat from "./DarkWhat";
 import What from "./What";
 import Search from "./Search";
 import SmallHeart from "./SmallHeart";
+import Checkbox from "./Checkbox";
+import UnCheckbox from "./UnCheckbox";
+import LogoMobile from "./LogoMobile";
+import FloatingButton from "./FloatingButton";
+import DarkCommunity from "./DarkCommuity";
+import Community from "./Community";
+import ArrowLeft from "./ArrowLeft";
+import MyEdit from "./MyEdit";
+import MyBookmark from "./MyBookmark";
+import MyNotice from "./MyNotice";
+import Mypost from "./MyPost";
 
 export {
   Duck,
+  MyBookmark,
+  MyNotice,
+  MyEdit,
+  Mypost,
+  ArrowLeft,
   ShowEyes,
   DarkWhere,
   Where,
@@ -20,4 +36,10 @@ export {
   What,
   Search,
   SmallHeart,
+  Checkbox,
+  UnCheckbox,
+  LogoMobile,
+  FloatingButton,
+  DarkCommunity,
+  Community,
 };
