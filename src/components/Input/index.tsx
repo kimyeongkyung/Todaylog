48bf76f905e85100185ecfaa440cbfb0ba49d728
kimyeongkyung <klk17625@naver.com>
2023-12-19 01:39:10 +0900

    Input 컴포넌트 생성

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid #ffc700;
+  width: 100%;
+  height: 48px;
+  border-radius: 5px;
+`;
+export default Input;
