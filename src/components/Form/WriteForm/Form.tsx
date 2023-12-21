ab249898bc9c9e21daaf4370c26fc5b8c6ac39aa
kimyeongkyung <klk17625@naver.com>
2023-12-21 17:11:33 +0900

    게시글 작성 페이지 formData 전송 형태로 구현

---
+++ src/components/Form/WriteForm/Form.tsx
@@ -0,0 +1,19 @@
+import { FirstPlace, LastPlace, SecondPlace, TitleWrite } from "../components";
+
+const TitleForm = () => {
+  return <TitleWrite />;
+};
+
+const FirstPlaceForm = () => {
+  return <FirstPlace />;
+};
+
+const SecondPlaceForm = () => {
+  return <SecondPlace />;
+};
+
+const LastPlaceForm = () => {
+  return <LastPlace />;
+};
+
+export { TitleForm, FirstPlaceForm, SecondPlaceForm, LastPlaceForm };
