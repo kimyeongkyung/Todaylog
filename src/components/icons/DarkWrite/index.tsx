c9c64cb5687e16d7bb6b3ec291a36527da8bb59a
kimyeongkyung <klk17625@naver.com>
2024-01-11 02:39:24 +0900

    필요한 Icons 추가

--- src/components/icons/DarkWrite/index.tsx
+++ src/components/icons/DarkWrite/index.tsx
@@ -12,16 +12,16 @@ const DarkWrite = () => {
       <path
         d="M12.5 4H3.55556C2.87778 4 2.22776 4.26925 1.7485 4.7485C1.26925 5.22776 1 5.87778 1 6.55556V24.4444C1 25.1222 1.26925 25.7722 1.7485 26.2515C2.22776 26.7308 2.87778 27 3.55556 27H21.4444C22.1222 27 22.7722 26.7308 23.2515 26.2515C23.7308 25.7722 24 25.1222 24 24.4444V15.5"
         stroke="#D4D4D4"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22.384 1.79198C22.8911 1.28488 23.5789 1 24.296 1C25.0132 1 25.7009 1.28488 26.208 1.79198C26.7151 2.29907 27 2.98684 27 3.70398C27 4.42112 26.7151 5.10889 26.208 5.61598L14.0987 17.7253L9 19L10.2747 13.9013L22.384 1.79198Z"
         stroke="#D4D4D4"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- src/components/icons/MyNotice/index.tsx
+++ src/components/icons/MyNotice/index.tsx
@@ -9,25 +9,25 @@ const MyNotice = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_862_10167)">
+      <g clipPath="url(#clip0_862_10167)">
         <path
           d="M17 2.1377V28.8618C11.7937 28.8618 7.92551 22.062 7.92551 22.062H3.6125C3.21799 22.062 2.83964 21.9056 2.56068 21.6272C2.28172 21.3487 2.125 20.9711 2.125 20.5774V10.3108C2.125 9.91707 2.28172 9.53943 2.56068 9.261C2.83964 8.98257 3.21799 8.82615 3.6125 8.82615H7.92551C7.92551 8.82615 11.7937 2.1377 17 2.1377Z"
           stroke="#FFC700"
-          stroke-width="3"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinejoin="round"
         />
         <path
           d="M23.375 8.55176C23.7071 8.98306 24.007 9.46413 24.2696 9.98696C25.0726 11.5879 25.5035 13.519 25.5 15.5C25.5 17.549 25.0505 19.4428 24.2898 20.9737C24.0223 21.5117 23.7156 22.0061 23.375 22.4483"
           stroke="#FFC700"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M24.4375 27.7929C28.8921 25.1753 31.875 20.3819 31.875 14.903C31.875 9.50889 28.9835 4.78076 24.6439 2.1377"
           stroke="#FFC700"
-          stroke-width="3"
-          stroke-linecap="round"
+          strokeWidth="3"
+          strokeLinecap="round"
         />
       </g>
       <defs>
